bd3d29a59fab15a01db7240302a2c1a3e812e30d
ragnarlodbrok1992 <maraus92@gmail.com>
2023-04-28 19:51:37 +0200

    WIP

---
+++ design/button.py
@@ -0,0 +1,16 @@
+DESIGN_DOC = f"""
+  UI Element - button.
+    Clickable 2D HUD element with area with attached callback function, three render states and a label.
+  1. Three render states - unfocused, focused, clicked
+  2. Outside area
+  3. Inside area
+  4. In between areas there is a shading and light reflections
+  5. Shading changes in response to a state change.
+"""
+
+def design():
+    print(DESIGN_DOC)
+
+if __name__ == "__main__":
+    design()
+
