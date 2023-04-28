19ad49cc93514bd314da80ff10e8b90e0ac82cba
ragnarlodbrok1992 <maraus92@gmail.com>
2023-04-28 20:14:55 +0200

    WIP

--- design/button.py
+++ design/button.py
@@ -1,3 +1,11 @@
+"""
+Run design files from root of the project.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
 DESIGN_DOC = f"""
   UI Element - button.
     Clickable 2D HUD element with area with attached callback function, three render states and a label.
@@ -6,6 +14,8 @@ DESIGN_DOC = f"""
   3. Inside area
   4. In between areas there is a shading and light reflections
   5. Shading changes in response to a state change.
+
+  Attachment: design png.
 """
 
 def design():
@@ -13,4 +23,7 @@ def design():
 
 if __name__ == "__main__":
     design()
+    design_img = np.asarray(Image.open('design/button_design1.png'))
+    plt.imshow(design_img)
+    plt.show()
 
