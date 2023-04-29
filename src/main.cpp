8d2d53aa82f2f102105b26ca9446f64ad785f779
ragnarlodbrok1992 <maraus92@gmail.com>
2023-04-29 15:48:53 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,15 @@
 
 #include "cpp-ui-libraries.h"
 
+// Forward declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow* window);
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
+// Settings
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+const unsigned int _GL_MAJOR_VERSION = 3;
+const unsigned int _GL_MINOR_VERSION = 3;
 
 
 int main(int argc, char* argv[]) {
@@ -20,18 +25,20 @@ int main(int argc, char* argv[]) {
 
   // Hello OpenGL
   glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, _GL_MAJOR_VERSION);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, _GL_MINOR_VERSION);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "UI-Libraries", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "UI-Libraries", NULL, NULL);
   if (window == NULL)
   {
       std::cout << "Failed to create GLFW window" << std::endl;
       glfwTerminate();
       return -1;
   }
+
   glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
   {
@@ -39,11 +46,15 @@ int main(int argc, char* argv[]) {
       return -1;
   }    
 
-  glViewport(0, 0, 800, 600);
-
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
   while (!glfwWindowShouldClose(window)) {
+    // Input
+    process_input(window);
+
+    // Render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Buffer swapping - event polling
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -51,4 +62,13 @@ int main(int argc, char* argv[]) {
   glfwTerminate();
 
   return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
